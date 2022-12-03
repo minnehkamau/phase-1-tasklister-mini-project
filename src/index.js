77f833821e63bf6a229b6718f390716911ca7915
minnehkamau <minnieches08@gmail.com>
2022-12-03 23:42:55 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    buildToDo(e.target.new_task.value)
+  })
+})
+function buildToDo(mylist){
+ let p=document.createElement('p')
+  let btn=document.createElement('button')
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${mylist}`
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+};
